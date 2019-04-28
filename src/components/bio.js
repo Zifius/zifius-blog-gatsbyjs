da1d73a4ab677d1b1d259604d9c95da4bbbb1638
Alexander Turiak <oturiak@adobe.com>
2019-04-28 22:08:14 +0200

    Remove extra space from bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -40,7 +40,6 @@ function Bio() {
             <p>
               Blog by <strong>{author}</strong>, Technical Architect at Magento (an Adobe company)
                 Professional Services
-              {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 Follow him on Twitter
               </a>
