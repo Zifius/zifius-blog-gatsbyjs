a14a295f58221fe60830253ad8e27fa7bbdbec9a
Alexander Turiak <oturiak@adobe.com>
2019-04-28 15:26:55 +0200

    Initial configuration updates

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,11 +38,11 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Blog by <strong>{author}</strong> who works as Technical Architect at Magento (an Adobe company)
+                Professional Services
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                Follow him Twitter
               </a>
             </p>
           </div>
